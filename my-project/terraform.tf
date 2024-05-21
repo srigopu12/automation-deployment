4a3fcd76973f56980cc0ea561cad483cc1888d18
srigopu12 <161108057+srigopu12@users.noreply.github.com>
2024-05-21 23:55:02 +0530

    Update terraform.tf

--- my-project/terraform.tf
+++ my-project/terraform.tf
@@ -1,6 +1,9 @@
 provider "aws" {
   region = "ap-south-1"
+  access_key = "AKIAX55NQXNL3WDXNMOD"
+  secret_key = "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"
 }
+
 
 resource "aws_instance" "web" {
   ami           = "ami-0cc9838aa7ab1dce7"
